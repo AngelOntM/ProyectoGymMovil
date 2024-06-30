c8d56150335ebe4e8737e47cb3c84eb0341e6d8d
Gerardo Plata <gerardogplatau@gmail.com>
2024-06-30 05:41:14 -0600

    ApiClient: Cambiar a patrón Singleton y configurar OkHttpClient sin timeouts para evitar bloqueos

--- app/src/main/java/com/utt/gymbros/api/ApiClient.java
+++ app/src/main/java/com/utt/gymbros/api/ApiClient.java
@@ -1,13 +1,30 @@
 package com.utt.gymbros.api;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static Retrofit getInstance(){
-        return new Retrofit.Builder()
-                .baseUrl("http://192.168.100.2:8000/api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getInstance() {
+        if (retrofit == null) {
+            // Create OkHttpClient with no timeouts
+            OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                    .connectTimeout(0, TimeUnit.MILLISECONDS)
+                    .readTimeout(0, TimeUnit.MILLISECONDS)
+                    .writeTimeout(0, TimeUnit.MILLISECONDS)
+                    .build();
+
+            // Build Retrofit instance with the custom OkHttpClient
+            retrofit = new Retrofit.Builder()
+                    .baseUrl("http://192.168.100.2:8000/api/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient) // Use the custom client
+                    .build();
+        }
+        return retrofit;
     }
 }
